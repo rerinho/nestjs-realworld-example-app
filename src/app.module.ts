c8b96a1e30009be8466a6070d33eed160231d4f1
Harrison Lopes <harrisonlopeslb@gmail.com>
2021-06-13 17:07:52 -0300

    feat: Adicionando configuração de environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
 
+const ENV = process.env.NODE_ENV;
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${ENV}`],
+      isGlobal: true,
+      load: [appConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
