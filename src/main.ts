99d8b2b21bfe351025d397a1f22bc1152e679377
Harrison Lopes <harrisonlopeslb@gmail.com>
2021-06-13 22:53:50 -0300

    feat: Adicionando ValidationPipe e prefixo "api"

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
 
   await app.listen(configService.get('PORT'));
   console.log(`App listening on PORT: ${configService.get('PORT')}`);
