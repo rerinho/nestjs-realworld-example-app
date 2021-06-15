dff78aa8adeacc4f96eaf2b0509ee9c4faf348f4
Harrison Lopes <harrisonlopeslb@gmail.com>
2021-06-14 22:31:49 -0300

    fix: Corrigindo tipagem da hash e validação da criação do usuário

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,19 +2,19 @@ import { Injectable, BadRequestException } from '@nestjs/common';
 import { UserCreateDTO } from './dto';
 import { PrismaService } from '@shared/prisma.service';
 import { User } from '@prisma/client';
-import { IHash } from '@shared/utils/hash';
+import { Bcrypt } from '@shared/utils/hash';
 
 @Injectable()
 export class UserService {
   constructor(
     private readonly prisma: PrismaService,
-    private readonly hash: IHash,
+    private readonly hash: Bcrypt,
   ) {}
 
   async create(data: UserCreateDTO): Promise<User> {
     const user = await this.findByEmailOrUsername(data.email, data.username);
 
-    if (!user) {
+    if (user) {
       throw new BadRequestException(
         'Já existe um usuário com o email ou username informado.',
       );
