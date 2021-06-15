37c25f35630c48d42172bc29ece42f3757b52ace
Harrison Lopes <harrisonlopeslb@gmail.com>
2021-06-14 23:36:49 -0300

    db: Incluindo a migration inicial

--- prisma/migrations/20210615023552_init/migration.sql
+++ prisma/migrations/20210615023552_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "user" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "username" TEXT NOT NULL,
@@ -7,10 +7,13 @@ CREATE TABLE "User" (
     "bio" TEXT,
     "image" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
+CREATE UNIQUE INDEX "user.email_unique" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user.username_unique" ON "user"("username");
