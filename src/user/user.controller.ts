86353656bfd138c61f17fb34c725fcb5d7884a3a
Harrison Lopes <harrisonlopeslb@gmail.com>
2021-06-15 00:48:53 -0300

    feat: Adicionando rotas [GET] /user e [PUT] /user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserCreateDTO, UserReadDTO } from './dto';
+import { Body, Controller, Post, Put, Get } from '@nestjs/common';
+import { UserCreateDTO, UserReadDTO, UserUpdateDTO } from './dto';
 import { UserService } from './user.service';
 
 @Controller()
@@ -9,6 +9,18 @@ export class UserController {
   @Post('/users')
   async create(@Body('user') userData: UserCreateDTO) {
     const user = await this.userService.create(userData);
+    return new UserReadDTO(user);
+  }
+
+  @Put('/user')
+  async update(@Body('user') userData: UserUpdateDTO) {
+    const user = await this.userService.update(1, userData);
+    return new UserReadDTO(user);
+  }
+
+  @Get('/user')
+  async find() {
+    const user = await this.userService.findBy({ id: 1 });
     return new UserReadDTO(user);
   }
 }
