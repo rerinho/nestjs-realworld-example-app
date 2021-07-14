c3f28223031e1448e72eac78087e9c9a6d0de149
Harrison Lopes <harrisonlopeslb@gmail.com>
2021-07-14 00:10:47 -0300

    feat: adicionando novos dtos no index

--- src/article/dto/index.ts
+++ src/article/dto/index.ts
@@ -2,3 +2,6 @@ export * from './article-create.dto';
 export * from './article-update.dto';
 export * from './article-params.dto';
 export * from './article-read.dto';
+export * from './article-list-query.dto';
+export * from './article-feed-query.dto';
+export * from './article-list-read.dto';
