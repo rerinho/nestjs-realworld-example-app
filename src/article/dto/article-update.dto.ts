a6eac4944e831a3bb64d176e53d3ebd2a425a5c2
Harrison Lopes <harrisonlopeslb@gmail.com>
2021-07-13 22:40:59 -0300

    feat(ArticleUpdateDTO): adicionando PartialType do nest

--- src/article/dto/article-update.dto.ts
+++ src/article/dto/article-update.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, OmitType } from '@nestjs/mapped-types';
 import { ArticleCreateDTO } from './';
 
-export class ArticleUpdateDTO extends PartialType(ArticleCreateDTO) {}
+export class ArticleUpdateDTO extends PartialType(
+  OmitType(ArticleCreateDTO, ['tagList'] as const),
+) {}
