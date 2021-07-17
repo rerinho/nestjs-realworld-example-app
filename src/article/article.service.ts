39234afd50f572ac7b20128c7723a375e4c9b5b3
Harrison Lopes <harrisonlopeslb@gmail.com>
2021-07-17 02:26:55 -0300

    feat: - adicionando métodosfavorite e unfavorite; - incluindo montagem do where dinâmico no método findAll

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,7 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '~/shared/prisma.service';
-import { ArticleCreateDTO, ArticleUpdateDTO } from './dto';
+import {
+  ArticleCreateDTO,
+  ArticleFeedQueryDTO,
+  ArticleListQueryDTO,
+  ArticleUpdateDTO,
+} from './dto';
+import { ArticleNotFoundException } from './exception';
 
+const DEFAULT_LIMIT = 20;
+const DEFAULT_OFFSET = 0;
 @Injectable()
 export class ArticleService {
   constructor(private readonly prisma: PrismaService) {}
@@ -23,6 +31,7 @@ export class ArticleService {
       data: articleData,
       include: {
         author: true,
+        tagList: true,
       },
     });
   }
@@ -40,26 +49,91 @@ export class ArticleService {
     });
   }
 
-  async findBySlug(slug: string) {
-    return this.prisma.article.findUnique({
+  async findBySlug(slug: string, includeFavoritedBy = false) {
+    const article = await this.prisma.article.findUnique({
       where: {
         slug,
       },
       include: {
         author: true,
+        tagList: true,
+        favoritedBy: includeFavoritedBy,
       },
     });
+
+    if (!article) {
+      throw new ArticleNotFoundException();
+    }
+
+    return article;
   }
 
-  async findAll() {
+  async findAll(articleListQueryDTO: ArticleListQueryDTO) {
+    const { limit = DEFAULT_LIMIT, offset = DEFAULT_OFFSET } =
+      articleListQueryDTO;
+
+    const where = this._buildFindAllWhere(articleListQueryDTO);
+
     return this.prisma.article.findMany({
+      where,
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: Number(limit),
+      skip: Number(offset),
       include: {
         author: true,
+        tagList: true,
       },
     });
   }
 
-  async feed(userId: number) {
+  private _buildFindAllWhere({ author, tag, favorited }: ArticleListQueryDTO) {
+    const queries = [];
+
+    if (author) {
+      queries.push({
+        author: {
+          username: {
+            equals: author,
+          },
+        },
+      });
+    }
+
+    if (tag) {
+      queries.push({
+        tagList: {
+          some: {
+            name: {
+              equals: tag,
+            },
+          },
+        },
+      });
+    }
+
+    if (favorited) {
+      queries.push({
+        favoritedBy: {
+          some: {
+            username: {
+              equals: favorited,
+            },
+          },
+        },
+      });
+    }
+
+    return {
+      AND: queries,
+    };
+  }
+
+  async feed(
+    userId: number,
+    { limit = DEFAULT_LIMIT, offset = DEFAULT_OFFSET }: ArticleFeedQueryDTO,
+  ) {
     return this.prisma.article.findMany({
       where: {
         author: {
@@ -70,11 +144,14 @@ export class ArticleService {
           },
         },
       },
+      take: Number(limit),
+      skip: Number(offset),
       orderBy: {
         createdAt: 'desc',
       },
       include: {
         author: true,
+        tagList: true,
       },
     });
   }
@@ -82,7 +159,7 @@ export class ArticleService {
   async update(
     slug: string,
     userId: number,
-    articleUpdateDTO: ArticleUpdateDTO,
+    articleUpdateDTO: ArticleUpdateDTO = null,
   ) {
     const article = await this.prisma.article.findFirst({
       where: {
@@ -94,7 +171,7 @@ export class ArticleService {
     });
 
     if (!article) {
-      throw new NotFoundException('Artigo não encontrado');
+      throw new ArticleNotFoundException();
     }
 
     return this.prisma.article.update({
@@ -104,6 +181,7 @@ export class ArticleService {
       data: articleUpdateDTO,
       include: {
         author: true,
+        tagList: true,
       },
     });
   }
@@ -112,6 +190,62 @@ export class ArticleService {
     return this.prisma.article.delete({
       where: {
         slug,
+      },
+    });
+  }
+
+  async favorite(slug: string, userId: number) {
+    const article = await this.findBySlug(slug, true);
+
+    const hasFavorited = article.favoritedBy.some((user) => user.id === userId);
+
+    if (hasFavorited) {
+      return article;
+    }
+
+    return this.prisma.article.update({
+      where: {
+        slug,
+      },
+      data: {
+        favoritedBy: {
+          connect: {
+            id: userId,
+          },
+        },
+        favoritesCount: ++article.favoritesCount,
+      },
+      include: {
+        author: true,
+        tagList: true,
+      },
+    });
+  }
+
+  async unfavorite(slug: string, userId: number) {
+    const article = await this.findBySlug(slug, true);
+
+    const hasFavorited = article.favoritedBy.some((user) => user.id === userId);
+
+    if (!hasFavorited) {
+      return article;
+    }
+
+    return this.prisma.article.update({
+      where: {
+        slug,
+      },
+      data: {
+        favoritedBy: {
+          disconnect: {
+            id: userId,
+          },
+        },
+        favoritesCount: --article.favoritesCount,
+      },
+      include: {
+        author: true,
+        tagList: true,
       },
     });
   }
